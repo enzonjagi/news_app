d096c5e1b882bf5545be9924d6d8b498880464a8
enzonjagi <amosndungo@gmail.com>
2022-08-09 12:47:10 +0300

    Clean code setup complete

---
+++ lib/src/core/utils/constants.dart
@@ -0,0 +1,9 @@
+const String kMaterialAppTitle = 'Flutter Clean Architecture';
+
+//API
+const String kBaseUrl = 'https://newsapi.org/v2';
+const String kApiKey = '9b343928ce994596ae3bbf51e0b17120';
+
+//Database
+const String kArticlesTableName = 'articles_table';
+const String kDatabaseName = 'app_database.db';
